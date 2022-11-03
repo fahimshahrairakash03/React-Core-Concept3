38bfd47e07ae0e7858a7febc9e3cdaef8a833e98
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-11-03 10:40:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <LoadComments></LoadComments>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handlerIncrease = () => setCount(count + 1);
+  const handlerDecrease = () => setCount(count - 1);
+
+  return (
+    <div>
+      <h1>Counter: {count} </h1>
+      <button onClick={handlerIncrease}>Increase</button>
+      <button onClick={handlerDecrease}>Decrease</button>
+    </div>
+  );
+}
+
+function LoadComments() {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Comments: {comments.length}</h2>
+      {comments.map((comment) => (
+        <Comments email={comment.email} body={comment.body}></Comments>
+      ))}
+    </div>
+  );
+}
+
+function Comments(props) {
+  return (
+    <div>
+      <h4 style={{ color: "red" }}>Email: {props.email}</h4>
+      <p>{props.body}</p>
     </div>
   );
 }
